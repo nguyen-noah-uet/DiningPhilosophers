793d8af951e9abd556a3d0fb227631e0d7c64985
Nguyen Khai Hoan <20020666@vnu.edu.vn>
2022-09-25 15:24:21 +0700

    Split files

--- DiningPhilosophers/Program.cs
+++ DiningPhilosophers/Program.cs
@@ -21,13 +21,3 @@ public class Program
 
 	}
 }
-
-public class Philosopher
-{
-	public int Id { get; set; }
-	public int? LeftChopstick { get; set; } = null;
-	public int? RightChopstick { get; set; } = null;
-	public int ExpectedLeftChopstick => (Id + 1) % 5;
-	public int ExpectedRightChopstick => Id;
-	public bool IsEating => LeftChopstick != null && RightChopstick != null;
-}
